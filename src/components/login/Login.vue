6eda645f78a472bf66ba82be92a93c0a24897d34
wp <wellpeng97@qq.com>
2019-11-28 15:43:23 +0800

    使用async和await简化异步代码

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -30,27 +30,32 @@
     },
     methods: {
       // 登录请求
-      handleLogin () {
-        this.$http.post('login', this.formData).then(res => {
-          // console.log(res)
-          const {
-            data,
-            meta: {msg, status}
-          } = res.data
+      async handleLogin () {
+        // 异步请求原代码
+        // this.$http.post('login', this.formData).then((res) => {...})
+        // 代码简化
+        // async+await 使异步代码ajax看起来像同步代码
+        // 1. 找到异步操作有结果的代码 前面家await 同时接口异步操作的结果res
+        // 2. 找到距离异步操作有结果最近的方法 前面加async
+        const res = await this.$http.post('login', this.formData)
+        // console.log(res)
+        const {
+          data,
+          meta: {msg, status}
+        } = res.data
 
-          if (status === 200) {
-            // 登录成功
-            // 1. 跳转home
-            this.$router.push({name: 'Home'})
-            // 2. 提示成功
-            this.$message.success(msg)
-            console.log(data)
-          } else {
-            // 登录失败
-            // 提示错误信息
-            this.$message.error(msg)
-          }
-        })
+        if (status === 200) {
+          // 登录成功
+          // 1. 跳转home
+          this.$router.push({name: 'Home'})
+          // 2. 提示成功
+          this.$message.success(msg)
+          console.log(data)
+        } else {
+          // 登录失败
+          // 提示错误信息
+          this.$message.error(msg)
+        }
       }
     }
   }
