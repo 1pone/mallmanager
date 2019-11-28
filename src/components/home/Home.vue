255d1bf09e473456918932c4c7cbea215ce066a5
wp <wellpeng97@qq.com>
2019-11-28 17:59:24 +0800

    导航菜单设置及home组件token验证

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -18,7 +18,82 @@
           </el-row>
         </el-header>
         <el-container>
-          <el-aside  class="aside" width="200px">Aside</el-aside>
+          <el-aside  class="aside" width="200px">
+            <el-menu default-active="1" unique-opened router>
+              <el-submenu index="1">
+                <template slot="title">
+                  <i class="el-icon-user-solid"></i>
+                  <span>用户管理</span>
+                </template>
+                <el-menu-item-group>
+                  <el-menu-item index="1-1">
+                    <i class="el-icon-document"></i>
+                    <span>用户列表</span>
+                  </el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+              <el-submenu index="2">
+                <template slot="title">
+                  <i class="el-icon-key"></i>
+                  <span>权限管理</span>
+                </template>
+                <el-menu-item-group>
+                  <el-menu-item index="2-1">
+                    <i class="el-icon-document"></i>
+                    <span>角色列表</span>
+                  </el-menu-item>
+                  <el-menu-item index="2-2">
+                    <i class="el-icon-document"></i>
+                    <span>权限列表</span>
+                  </el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+              <el-submenu index="3">
+                <template slot="title">
+                  <i class="el-icon-shopping-cart-1"></i>
+                  <span>商品管理</span>
+                </template>
+                <el-menu-item-group>
+                  <el-menu-item index="3-1">
+                    <i class="el-icon-document"></i>
+                    <span>商品列表</span>
+                  </el-menu-item>
+                  <el-menu-item index="3-2">
+                    <i class="el-icon-cpu"></i>
+                    <span>分类参数</span>
+                  </el-menu-item>
+                  <el-menu-item index="3-3">
+                    <i class="el-icon-set-up"></i>
+                    <span>商品分类</span>
+                  </el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+              <el-submenu index="4">
+                <template slot="title">
+                  <i class="el-icon-receiving"></i>
+                  <span>订单管理</span>
+                </template>
+                <el-menu-item-group>
+                  <el-menu-item index="4-1">
+                    <i class="el-icon-document"></i>
+                    <span>订单列表</span>
+                  </el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+              <el-submenu index="5">
+                <template slot="title">
+                  <i class="el-icon-s-data"></i>
+                  <span>数据统计</span>
+                </template>
+                <el-menu-item-group>
+                  <el-menu-item index="5-1">
+                    <i class="el-icon-pie-chart"></i>
+                    <span>数据报表</span>
+                  </el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+            </el-menu>
+          </el-aside>
           <el-main class="main">Main</el-main>
         </el-container>
       </el-container>
@@ -29,6 +104,15 @@
     name: 'Home',
     data () {
       return {}
+    },
+    // 组件渲染之前检查token
+    beforeCreate () {
+      const token = localStorage.getItem('token')
+      if (!token) {
+        // 没有token，跳转login页面
+        this.$router.push({name: 'Login'})
+      }
+    //  有token 继续渲染页面
     }
   }
 </script>
