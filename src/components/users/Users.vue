b57df415d03953cae6f44a9e9e86bdc9c4452ae6
wp <wellpeng97@qq.com>
2019-12-01 17:56:33 +0800

    分页，用户搜索，添加用户

--- src/components/users/Users.vue
+++ src/components/users/Users.vue
@@ -14,10 +14,15 @@
 <!--      2. 搜索框-->
       <el-row style="margin-top: 15px" :gutter="20">
         <el-col>
-          <el-input placeholder="搜索用户" v-model="query" style="max-width: 300px; margin-right: 15px">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input
+            placeholder="搜索用户"
+            v-model="query" clearable
+            @clear="getUserList"
+            @change="getUserList"
+            style="max-width: 300px; margin-right: 15px">
+            <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
           </el-input>
-          <el-button type="primary">添加用户</el-button>
+          <el-button type="primary" @click="dialogFormVisible = true">添加用户</el-button>
         </el-col>
       </el-row>
 <!--      3. 用户表格-->
@@ -84,6 +89,42 @@
           </el-table-column>
         </el-table>
       </el-row>
+<!--      4. 分页-->
+      <el-row>
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="pageNum"
+          :page-sizes="page_sizes"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total">
+        </el-pagination>
+      </el-row>
+<!--      添加用户对话框表单-->
+      <el-dialog title="添加用户" :visible.sync="dialogFormVisible" @close="dialogClose('form')">
+        <el-form :model="form" :rules="rules" ref="form" status-icon>
+          <el-form-item label="用户名" prop="username" :label-width="labelWidth">
+            <el-input v-model="form.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password" :label-width="labelWidth">
+            <el-input v-model="form.password" type="password"></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码" prop="passwordConfirm" :label-width="labelWidth">
+            <el-input v-model="form.passwordConfirm" type="password"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email" :label-width="labelWidth">
+            <el-input v-model="form.email"></el-input>
+          </el-form-item>
+          <el-form-item label="手机号" prop="mobile" :label-width="labelWidth">
+            <el-input v-model="form.mobile"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogClose('form')">取 消</el-button>
+          <el-button type="primary" @click="submitForm('form')">确 定</el-button>
+        </div>
+      </el-dialog>
     </el-card>
   </div>
 </template>
@@ -92,20 +133,102 @@
   export default {
     name: 'Users',
     data () {
+      // 在 data() 中，且 return 之前，自定义一些校验规则
+      // 校验密码
+      const validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'))
+        } else {
+          if (this.form.passwordConfirm !== '') {
+            // 当输入密码后密码校验框的值为非空时，立刻执行passwordConfirm字段校验
+            this.$refs.form.validateField('passwordConfirm') // validateField() 对部分表单字段进行校验的方法
+          }
+          // 校验通过
+          callback()
+        }
+      }
+
+      // 二次校验密码
+      const validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'))
+        } else if (value !== this.form.password) {
+          callback(new Error('两次输入密码不一致!'))
+        } else {
+          callback()
+        }
+      }
+
+      // 校验邮箱
+      const checkEmail = (rule, value, callback) => {
+        if (/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(value) === false && value !== '') {
+          return callback(new Error('邮箱地址不正确'))
+        }
+        callback()
+      }
+
+      // 校验手机号
+      const checkMobile = (rule, value, callback) => {
+        if (/^1\d{10}$/.test(value) === false && value !== '') {
+          return callback(new Error('手机号不正确'))
+        }
+        callback()
+      }
+
       return {
-        query: '',
-        // 分页相关数据
-        pageNum: 1,
-        pageSize: 10,
-        total: 0,
+        query: '', // 查询参数
         // 表格绑定的数据
-        userList: []
+        userList: [], // 用户列表
+        // 对话框表单
+        dialogFormVisible: false,
+        labelWidth: '120px',
+        // 添加用户表单请求参数
+        // 参数名 参数说明 备注
+        // username 用户名称 不能为空
+        // password 用户密码 不能为空
+        // email 邮箱 可以为空
+        // mobile 手机号 可以为空
+        form: {
+          username: '',
+          password: '',
+          passwordConfirm: '',
+          email: '',
+          mobile: ''
+        },
+        // 表单规则
+        rules: {
+          username: [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min: 2, max: 8, message: '长度在 2 到 8 个字符', trigger: 'blur' }
+          ],
+          password: [
+            { validator: validatePass, trigger: 'blur' },
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' }
+          ],
+          passwordConfirm: [
+            { validator: validatePass2, trigger: 'blur' },
+            { required: true, message: '请再次输入密码', trigger: 'blur' }
+          ],
+          email: [
+            { validator: checkEmail, trigger: 'blur' }
+          ],
+          mobile: [
+            { validator: checkMobile, trigger: 'blur' }
+          ]
+        },
+        // 分页相关数据
+        pageNum: 1, //
+        pageSize: 2, // 每页数据条数
+        page_sizes: [2, 4, 6, 8], // 数据条数集合
+        total: 0 // 数据总条数
       }
     },
     created () {
       this.getUserList()
     },
     methods: {
+      // 获取用户数据列表
       async getUserList () {
         // 参数名 参数说明 备注
         // query 查询参数 可以为空
@@ -127,6 +250,64 @@
         } else {
           this.$message.error(msg)
         }
+      },
+
+      // 关闭添加用户对话框
+      dialogClose (formName) {
+        this.dialogFormVisible = false
+        this.$refs[formName].resetFields()
+        // 清空表单数据
+        // 方法一 直接赋值空对象，在input时重新创建对象
+        // this.form = {}
+        // 方法二 遍历清空数组
+        // for (let key in this.form) {
+        //   if (this.form.hasOwnProperty(key)) {
+        //     this.form[key] = ''
+        //   }
+        // }
+        // 方法三 重新赋值
+        // this.form = {username: '', password: '', email: '', mobile: ''}
+        // 方法四 this.$refs[formName].resetFields()
+      },
+
+      // 添加用户
+      async addUser (formName) {
+        const res = await this.$http.post('/users', this.form)
+        const {data, meta: {msg, status}} = res.data
+        if (status === 201) {
+          console.log(data)
+          this.getUserList()
+          this.$message.success(msg)
+        } else {
+          this.$message.error(msg)
+        }
+        this.dialogClose(formName)
+      },
+
+      // 表单确认
+      submitForm (formName) {
+        this.$refs[formName].validate(valid => {
+          if (valid) {
+            this.addUser(formName)
+            console.log('submit!')
+          } else {
+            console.log('error submit!')
+            return false
+          }
+        })
+      },
+
+      // 列表分页
+      // 1. 修改每页数据显示条数
+      handleSizeChange (val) {
+        this.pageSize = val
+        this.pageNum = 1 // 每页数据条数改变时重新返回第一页
+        this.getUserList()
+      },
+      // 2. 修改当前页码
+      handleCurrentChange (val) {
+        this.pageNum = val
+        this.getUserList()
       }
     }
   }
